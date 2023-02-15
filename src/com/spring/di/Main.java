6efd63d5ebb7afc85bab26b436f6178c13d099d8
Abdallah Saleh <72446432+Abdallahsa@users.noreply.github.com>
2023-02-15 09:34:33 +0200

    scope bean

--- src/com/spring/di/Main.java
+++ src/com/spring/di/Main.java
@@ -11,6 +11,10 @@ public class Main {
 		ClassPathXmlApplicationContext cont=new ClassPathXmlApplicationContext("applicationContext.xml");
 			
 	        userInterface s2=cont.getBean("user",userInterface.class);
+	        userInterface s1=cont.getBean("user",userInterface.class);
+	        System.out.println(s1);
+	        System.out.println(s2);
+	        System.out.println(s1==s2);
 	        s2.start();
 	        s2.getperson();
 	        System.out.print(s2.getEmail()+" "+s2.getTeam());
